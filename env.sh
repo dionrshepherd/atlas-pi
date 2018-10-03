9ee74cef34f4dc4aaaa9ed7bec63153ae96ff11d
Dion Shepherd <dion@roamltd.com>
2018-10-03 15:52:18 +1300

    added more

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # set the environment variables that are needed
-ANCHOR_ID="$(hostname)"
+export ANCHOR_ID="$(hostname)"
 echo $ANCHOR_ID
 
-echo "run: source ~/atlas-pi/.bashrc"
+echo "run install.sh"
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-#pip3 install setuptools
-#pip3 install awscli
-#pip3 install boto3
-#pip3 install pyserial
+pip3 install setuptools
+pip3 install awscli
+pip3 install boto3
+pip3 install pyserial
