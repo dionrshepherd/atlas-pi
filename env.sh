53279ba1b010270d7b173f83bb4653b8d7098ad1
Dion Shepherd <dion@roamltd.com>
2018-10-04 09:05:32 +1300

    added in more prompts

--- env.sh
+++ env.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # set the environment variables that are needed
+echo "Has a new user been added?"
+echo "Have you changed the hostname to the anchor ID?"
+echo "/etc/hostname & /etc/hosts"
+
 export ANCHOR_ID="$(hostname)"
 echo $ANCHOR_ID
 
--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Have you sourced the env script first?"
+
 pip3 install setuptools
 pip3 install awscli==1.11.18
 pip3 install boto3
 pip3 install pyserial
+
+echo "configure aws"
