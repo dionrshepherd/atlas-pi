1ac3c602b88cf87dad0765815309711992e84551
Dion Shepherd <dion@roamltd.com>
2018-10-04 15:14:18 +1300

    fixed the path to the aws cli

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@ set -e
 
 echo "Have you sourced the env script first?"
 
+apt install python3-pip
 pip3 install setuptools
 pip3 install awscli==1.11.18
 pip3 install boto3
 pip3 install pyserial
 
-echo "configure aws"
+echo "run: ./.local/bin/aws configure"
