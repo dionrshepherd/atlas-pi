68a6124af3ec6e5551264af89c3dbad6d7dfe3f8
Dion Shepherd <dion@roamltd.com>
2018-11-06 09:30:17 +1300

    debugging inits

--- atlas-init.sh
+++ atlas-init.sh
@@ -8,17 +8,21 @@ touch /var/lock/atlas-init.sh
 case "$1" in
     start)
         # bind sensor to usb
-        echo '1-1.4' | sudo tee /sys/bus/usb/drivers/usb/bind
+        echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind
 
         # run the produce script
         python3 /home/linaro/atlas-pi/pi_produce.py
+
+        touch /home/linaro/start.txt
         ;;
     stop)
         # kill any running python scripts
         pkill -9 -f pi_produce.py
 
         # unbind sensor from usb
-         echo '1-1.4' | sudo tee /sys/bus/usb/drivers/usb/unbind
+        echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind
+
+        touch /home/linaro/stop.txt
         ;;
     *)
         echo "Usage: /etc/init.d/atlas-init.sh {start|stop}"
