d6112d38531828034f0dfea94268a88d74c11e63
Dion Shepherd <dion@roamltd.com>
2018-11-09 11:45:59 +1300

    changed debug logs

--- lambda_get_pos.py
+++ lambda_get_pos.py
@@ -18,13 +18,13 @@ def lambda_handler(event, context):
     logger['uuid'] = uid
     logger['level'] = ''
     logger['message'] = ''
-    print(json.dumps(event['Records']))
 
     for record in event['Records']: # might remove this first call, just get the state of the db
         if record['eventName'] == 'REMOVE':
             continue
 
         tag_id = record['dynamodb']['Keys']['id']['S']
+        logger['tagId'] = tag_id
 
         if len(tag_id) == 4:
             if tag_id in str(found_tags):
@@ -33,44 +33,49 @@ def lambda_handler(event, context):
                 response = table.scan(
                     FilterExpression=Attr('id').eq(tag_id)
                 )
+                if len(response['Items'][0]) > 0:
+                    item = response['Items'][0] # is an array, should only return one, so take the first
 
-                print(response)
+                    try:
+                        x = float(item['payload']['x'])
+                        y = float(item['payload']['y'])
+                        z = float(item['payload']['z'])
+                    except(ValueError):
+                        logger['level'] = 'ERROR'
+                        logger['message'] = 'Number Value Error:{}'.format(item)
+                        print(json.dumps(logger))
+                        continue
 
-                item = response['Items'][0] # is an array, should only return one, so take the first
-                logger['tagId'] = tag_id
+                    tag = {
+                        "tag": tag_id,
+                        "x": x,
+                        "y": y,
+                        "z": z,
+                        "ts": str(time.time())
+                    }
 
-                try:
-                    x = float(item['payload']['x'])
-                    y = float(item['payload']['y'])
-                    z = float(item['payload']['z'])
-                except(ValueError):
-                    logger['level'] = 'ERROR'
-                    logger['message'] = 'Number Value Error:{}'.format(item)
+                    found_tags.append(tag_id)
+
+                    iot_data_client.publish(
+                        topic='atlasDevTagCoords',
+                        payload=json.dumps(tag)
+                    )
+
+                    sns_client.publish(
+                        TopicArn='arn:aws:sns:ap-southeast-2:430634712358:atlas-proximity-event',
+                        Message=json.dumps(tag)
+                    )
+
+                    logger['level'] = 'SUCCESS'
+                    logger['message'] = 'successful MQTT and SNS publish'
+                    print(json.dumps(logger))
+                else:
+                    logger['level'] = 'WARNING'
+                    logger['message'] = 'TagId: {} has no entries in the database, this is probably due to malformed data and/or deleted rows'.format(tag_id)
                     print(json.dumps(logger))
                     continue
-
-                tag = {
-                    "tag": tag_id,
-                    "x": x,
-                    "y": y,
-                    "z": z,
-                    "ts": str(time.time())
-                }
-
-                found_tags.append(tag_id)
-
-                iot_data_client.publish(
-                    topic='atlasDevTagCoords',
-                    payload=json.dumps(tag)
-                )
-
-                sns_client.publish(
-                    TopicArn='arn:aws:sns:ap-southeast-2:430634712358:atlas-proximity-event',
-                    Message=json.dumps(tag)
-                )
-
-                logger['level'] = 'SUCCESS'
-                logger['message'] = 'successful MQTT and SNS publish'
-                print(json.dumps(logger))
         else:
+            logger['level'] = 'WARNING'
+            logger['message'] = 'TagId: {} is not the correct length, this is probably due to malformed data'.format(tag_id)
+            print(json.dumps(logger))
             continue
